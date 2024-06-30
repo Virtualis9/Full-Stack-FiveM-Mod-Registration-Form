57920601b3e2d2352bb2b532a8df6c08e7da577b
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2024-06-30 21:35:39 +0100

    if in install enough packages it will eventually work

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const multer = require('multer');
+const upload = multer();
 
 app.get('/', (req, res) => {
   res.send('Express is working!');
 });
 
-app.get('/index', (req, res)=>{
-    res.render()
-})
+
+app.post('/submit', upload.none(), (req, res) => {
+  console.log(req.body);
+  res.json({ message: 'Form submitted successfully', data: req.body });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
